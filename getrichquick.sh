46c196029414bcbcf7d7b6df460c9d9d1b637b22
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-08-16 12:27:44 -0700

    test

--- getrichquick.sh
+++
@@ -1,6 +0,0 @@
-echo "what is your name?"
-read name
-echo "what is your age?"
-read age
-ageOfMillionaire=$(($age + ($RANDOM % 15))) 
-echo "Hello, $name, you are going to become a millionaire at age $ageOfMillionaire."
